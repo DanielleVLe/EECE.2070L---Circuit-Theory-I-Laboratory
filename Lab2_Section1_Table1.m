3a130d7c0a96506d17f80630e308a792c0639035
Danielle <danielle.le04@gmail.com>
2020-10-20 17:15:05 -0400

    Add files via upload
    This is the calculation for The Ir=Vr/Rmeasured in Table 1 of Lab#2.

---
+++ Lab2_Section1_Table1.m
@@ -0,0 +1,26 @@
+%Section 1- Ohm’s Law (Determining I)
+
+%My data 
+a = [0, 2, 4, 6, 8, 10]; %Vr (VOM)-- Voltage going into circuit (x-axis)
+b = [0, 0.8, 1.2, 1.7, 2.4, 4.1]; %I (DMM)-- Current DMM measurements
+
+a1 = a(:,1)./b(:,1);
+a2 = a(:,2)./b(:,2);
+a3 = a(:,3)./b(:,3);
+a4 = a(:,4)./b(:,4);
+a5 = a(:,5)./b(:,5);
+a6 = a(:,6)./b(:,6);
+
+fprintf('a1=%f, a2 = %f, a3 = %f, a4 = %f, a5 = %f, a6= %f\n',a1,a2,a3,a4,a5,a6)
+
+
+
+
+
+
+
+
+
+
+
+
