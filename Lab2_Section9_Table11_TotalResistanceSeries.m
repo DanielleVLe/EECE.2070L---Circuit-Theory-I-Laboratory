b4868ed3ea91fc230d77b30f5f83002ce5e7e829
Danielle <danielle.le04@gmail.com>
2020-10-21 03:57:51 -0400

    Add files via upload

---
+++ Lab2_Section9_Table11_TotalResistanceSeries.m
@@ -0,0 +1,16 @@
+% Name:        Danielle Le 
+% Course:      EECE.2070L- Basic Electrical Engineering Laboratory I 
+% Lab #2:      Section 9- Different Levels of Resistance
+% Description: Calculating the total resistance in series using the
+% measured values and recording it in Table 11. 
+
+%All 5 Measured resistor values in series circuit(Ω)
+r1=98;
+r2=981;
+r3=97000; 
+
+%Sum of all resistors 
+rt= r1+r2+r3;
+
+%Print the total sum in cmd 
+fprintf('Total Resistance in Series: %g Ω',rt)
